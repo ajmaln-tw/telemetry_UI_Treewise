c3200eb5dd1b5c75196b0c9897cd7a2655d0eff2
Ajmal Nasumudeen <ajmal.nasumudeen@treeswise.com>
2023-04-24 18:53:46 +0530

    Changes apiURL

--- src/modules/profile/components/settings/VesselData.js
+++ src/modules/profile/components/settings/VesselData.js
@@ -45,7 +45,7 @@ const VesselData = (props) => {
                             <FormController statusError={true} errorName={errors?.vesselName} control="input2" label={"Vessel Name"} name="vesselName" mandatory={true} />
                         </Grid>
                         <Grid item xs={12} md={12} lg={12}>
-                            <FormController statusError={true} errorName={errors?.apiURl} control="input2" label={"Vessel API URL"} name="apiURl" mandatory={true} />
+                            <FormController statusError={true} errorName={errors?.apiURL} control="input2" label={"Vessel API URL"} name="apiURL" mandatory={true} />
                         </Grid>
                     </Grid>
                 </CardContent>
